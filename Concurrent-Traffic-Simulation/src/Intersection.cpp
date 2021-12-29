17ce4d1880121c84c029714232f2166334348225
Dumeetra <86459746+Dumeetra@users.noreply.github.com>
2021-12-29 16:09:10 +0200

    Update Intersection.cpp

--- Concurrent-Traffic-Simulation/src/Intersection.cpp
+++ Concurrent-Traffic-Simulation/src/Intersection.cpp
@@ -89,7 +89,7 @@ void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
 //}
     std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID() << " is granted entry." << std::endl;
 
-       _trafficLight.cycleThroughPhases();
+     //  _trafficLight.cycleThroughPhases();
   
   if( _trafficLight.getCurrentPhase()==TrafficLightPhase::red){
   _trafficLight.waitForGreen();
